eacd3e4d08def96b68845c1e6d12a6ba17c3a136
Christopher Pole <chris.pole@blackcatsolutions.co.uk>
2021-03-25 14:19:44 +0000

    Resolve Pixi dependancy issues

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const DIRECTIONAL_OFFSET_THRESHOLD = 80;
 
 //  Game renderer
 export const MAX_ENTITIES_COUNT = 200;
-export const ENTITY_SIZE = 120;
+export const ENTITY_SIZE = 200;
 export const ENTITY_MOVE_SPEED = ENTITY_SIZE / 4;
 export const ENTITY_FADE_SPEED = ENTITY_MOVE_SPEED / 6; //  Lower is slower relative to move speed
 export const UNDOING_SPEED_MULTIPLIER = 2; //  Multiply other animation speeds with this number when undoing
